71fb07d420bceb8500d341c12dbb24f8af83ff03
hassa <hassa@LAPTOP-U03CN6LR>
2025-07-04 23:57:52 +0330

    add draw function on board

---
+++ src/Whiteboard/wwwroot/js/site.js
@@ -0,0 +1,39 @@
+﻿
+
+const canvas = document.getElementById('whiteboard');
+const ctx = canvas.getContext('2d');
+let painting = false;
+let x1 = 0;
+let y1 = 0;
+
+function startPosition(e) {
+    painting = true;
+    x1 = e.clientX - canvas.offsetLeft;
+    y1 = e.clientY - canvas.offsetTop;
+}
+
+function endPosition() {
+    painting = false;
+    ctx.beginPath();
+}
+
+function draw(e) {
+    if (!painting) return;
+
+    const x = e.clientX - canvas.offsetLeft;
+    const y = e.clientY - canvas.offsetTop;
+
+    ctx.lineWidth = 5;
+    ctx.lineCap = 'round';
+    ctx.strokeStyle = '#000000';
+
+    ctx.lineTo(x, y);
+    ctx.stroke();
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+
+}
+
+canvas.addEventListener('mousedown', startPosition);
+canvas.addEventListener('mouseup', endPosition);
+canvas.addEventListener('mousemove', draw);
