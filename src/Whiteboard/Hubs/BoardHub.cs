783146d96094493628286f9e88cc3f1a2927e996
hassa <hassa@LAPTOP-U03CN6LR>
2025-07-05 00:51:03 +0330

    add fontColor and fontSize accessbility tool

--- src/Whiteboard/Hubs/BoardHub.cs
+++ src/Whiteboard/Hubs/BoardHub.cs
@@ -4,9 +4,9 @@ namespace Whiteboard.Hubs
 {
     public class BoardHub : Hub
     {
-        public async Task DrawingAsync(int x1, int y1, int x2, int y2)
+        public async Task DrawingAsync(int x1, int y1, int x2, int y2 , string fontColor , int fontSize)
         {
-            await Clients.Others.SendAsync("OnDrawingAsync", x1, y1, x2, y2);
+            await Clients.Others.SendAsync("OnDrawingAsync", x1, y1, x2, y2 , fontColor , fontSize);
         }
     }
 }
--- src/Whiteboard/wwwroot/js/site.js
+++ src/Whiteboard/wwwroot/js/site.js
@@ -1,14 +1,55 @@
-﻿var connection = new signalR.HubConnectionBuilder()
+﻿
+
+const canvas = document.getElementById('whiteboard');
+const ctx = canvas.getContext('2d');
+let painting = false;
+let x1 = 0;
+let y1 = 0;
+
+var fontColor = '#000000';
+var fontSize = 12;
+
+
+// colorPicker changed
+const colorPicker = document.getElementById("colorPicker");
+colorPicker.addEventListener("change", function () {
+    fontColor = this.value;
+    refreshTool();
+});
+
+
+// fontSize changed
+const fontSizeSelect = document.getElementById("fontSizeSelect");
+fontSizeSelect.addEventListener("change", function () {
+    fontSize = parseInt(this.value);
+    refreshTool();
+});
+
+
+const refreshTool = () => {
+
+    ctx.lineWidth = fontSize;
+    ctx.lineCap = 'round';
+    ctx.strokeStyle = fontColor;
+
+}
+
+// socket connection
+var connection = new signalR.HubConnectionBuilder()
     .withUrl("/board").build();
 
 connection.start().catch(function (err) {
     return console.error(err.toString());
 });
 
-connection.on("OnDrawingAsync", (x1, y1, x2, y2) => {
-    ctx.lineWidth = 5;
-    ctx.lineCap = 'round';
-    ctx.strokeStyle = '#000000';
+
+
+connection.on("OnDrawingAsync", (x1, y1, x2, y2, fcolor, fsize) => {
+
+    fontColor = fcolor;
+    fontSize = fsize;
+
+    refreshTool();
 
     ctx.beginPath();
     ctx.moveTo(x1, y1);
@@ -16,12 +57,6 @@ connection.on("OnDrawingAsync", (x1, y1, x2, y2) => {
     ctx.stroke();
     ctx.closePath();
 });
-
-const canvas = document.getElementById('whiteboard');
-const ctx = canvas.getContext('2d');
-let painting = false;
-let x1 = 0;
-let y1 = 0;
 
 function startPosition(e) {
     painting = true;
@@ -40,16 +75,12 @@ function draw(e) {
     const x = e.clientX - canvas.offsetLeft;
     const y = e.clientY - canvas.offsetTop;
 
-    ctx.lineWidth = 5;
-    ctx.lineCap = 'round';
-    ctx.strokeStyle = '#000000';
-
     ctx.lineTo(x, y);
     ctx.stroke();
     ctx.beginPath();
     ctx.moveTo(x, y);
 
-    connection.invoke("DrawingAsync", x1, y1, x, y)
+    connection.invoke("DrawingAsync", x1, y1, x, y , fontColor , fontSize)
         .catch(function (err) {
             return console.error(err.toString());
         });
@@ -58,6 +89,9 @@ function draw(e) {
     y1 = y;
 }
 
+refreshTool();
+
 canvas.addEventListener('mousedown', startPosition);
 canvas.addEventListener('mouseup', endPosition);
 canvas.addEventListener('mousemove', draw);
+
